6b9d837b089436b1d7b5cc7e712e6087b482c976
Djordje <djmihajlovic1@gmail.com>
2025-03-06 10:59:39 +0000

    initial commit

---
+++ knot_evol.py
@@ -0,0 +1,504 @@
+import numpy as np
+from numba import njit, set_num_threads
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.cm as cm 
+import matplotlib.colors as mcolors  
+from knot_init import *
+
+'''
+BFACF algorithm for polygonal lattice knot evolution.
+
+Key Features:
+    * Oriented lattice knots S^{1} in Z^{3}
+    * Writhe calculation (https://www.unige.ch/math/folks/cimasoni/writhe.pdf)
+    * MCMC evolution towards highly entangled configurations
+    * Visualization
+
+To Implement:
+    * Knotoids
+    * Bonded knotoids
+    * Proteins (spec. bonded knotoids (capture forces) ~ protein_init.py ~ load in protein from PDB and automate)
+    * S^{2} in Z^{4}, (S^{n} in Z^{n+2})?
+    * TQFT inspired things?
+'''
+
+@njit(parallel=True)
+def neighbours(array, point):
+    '''
+    Takes in array and specified point, outputs an array of neighbours of point and associated neighbour value.
+    '''
+
+    neighbour = np.empty((26, 4))
+
+    idx = 0
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            for dz in [-1, 0, 1]:
+
+                if dx == 0 and dy == 0 and dz == 0:
+                    continue
+
+                nx_1, ny_1, nz_1 = point[0].item() + dx, point[1].item() + dy, point[2].item() + dz
+
+                if 0<= nx_1 <array.shape[0] and 0<= ny_1 <array.shape[1] and 0<= nz_1 <array.shape[2]: 
+                    neighbour[idx] = [nx_1, ny_1, nz_1, array[nx_1][ny_1][nz_1]]
+                    idx += 1
+
+    return neighbour
+
+@njit(parallel=True)
+def restricted_neighbours(array, point):
+    '''
+    Takes in array and point in array and outputs an array of neighbours and neighbour value.
+    '''
+
+    neighbour = np.empty((6, 4))
+
+    idx = 0
+    for dx in [-1, 1]:
+        nx_1, ny_1, nz_1 = point[0].item() + dx, point[1].item(), point[2].item()
+
+        if 0<= nx_1 <array.shape[0]: 
+            neighbour[idx] = [nx_1, ny_1, nz_1, array[nx_1][ny_1][nz_1]]
+            idx += 1
+
+    for dy in [-1, 1]:
+        nx_1, ny_1, nz_1 = point[0].item(), point[1].item() + dy, point[2].item()
+
+        if 0<= ny_1 <array.shape[1]: 
+            neighbour[idx] = [nx_1, ny_1, nz_1, array[nx_1][ny_1][nz_1]]
+            idx += 1
+
+    for dz in [-1, 1]:
+        nx_1, ny_1, nz_1 = point[0].item(), point[1].item(), point[2].item() + dz
+
+        if 0<= nz_1 <array.shape[2]: 
+            neighbour[idx] = [nx_1, ny_1, nz_1, array[nx_1][ny_1][nz_1]]
+            idx += 1
+
+    return neighbour
+
+
+def orient(array):
+    '''
+    For fixing orientation: make copy of array, assign 1 to some point and N to one of its neighbours. Now impose that every point (excluding)
+    1 and N must have a neighbour with +1 value and -1 value of current value. 
+    Save both oriented and unoriented structure, use unoriented structure (just 1's) for some calcs.
+    '''
+
+    indicies = np.argwhere(array == 1)
+    oriented_structure = array.copy()
+    p1 = indicies[0]
+
+    vec_1 = np.empty((2, 3))
+
+    neighbourhood = neighbours(array, p1)
+    idx = 0
+
+    for i in neighbourhood:
+        if i[3] == 1:
+            vec_1[idx] = [i[0], i[1], i[2]]
+            idx += 1
+
+    vec_1 = vec_1.astype(int)
+
+    oriented_structure[vec_1[0][0]][vec_1[0][1]][vec_1[0][2]] = len(indicies)
+    oriented_structure[vec_1[1][0]][vec_1[1][1]][vec_1[1][2]] = 2
+
+    neighbourhood = neighbours(oriented_structure, vec_1[1])
+
+    update_number = 3
+
+    neighbourhood = neighbourhood.astype(int)
+
+    for i in neighbourhood:
+        if i[3] == 1 and (i[0] != p1[0] or i[1] != p1[1] or i[2] != p1[2]):
+            oriented_structure[i[0]][i[1]][i[2]] = update_number
+            prev_number = update_number
+            update_number += 1
+        
+    while len(np.argwhere(oriented_structure ==1)) > 1:
+
+        index = np.argwhere(oriented_structure == prev_number)
+
+        for ind in index:
+            neighbourhood = neighbours(oriented_structure, ind)
+            neighbourhood = neighbourhood.astype(int)
+            for i in neighbourhood:
+                if i[3] == 1:
+                    oriented_structure[i[0]][i[1]][i[2]] = update_number
+                    prev_number = update_number
+                    update_number += 1
+
+    return oriented_structure
+
+
+@njit(parallel=True)
+def find_new(array, edge):
+    '''
+    Find valid locations to move edge, nb. needs to be restricted neighbours
+    '''
+
+    valid_neighbours = []
+
+    neighbourhood = restricted_neighbours(array, edge)
+
+    for i in neighbourhood:
+        if i[3] != 1:
+            valid_neighbours.append(i)
+    
+    new_edge = np.random.choice(len(valid_neighbours))
+
+    return valid_neighbours[new_edge]
+
+@njit(parallel=True)
+def check_double_edge(array):
+    '''
+    Checks for singular points.
+    '''
+    indicies = np.argwhere(array > 1)
+    
+    if len(indicies) != 0:
+        for i in indicies:
+            array[i[0]][i[1]][i[2]] = 0
+
+    return array
+
+@njit(parallel=True)
+def check_verticies(array):
+    '''
+    Checks the verticies of the 3D state space.
+    '''
+    indicies = np.argwhere(array > 0)
+
+    status = 0
+
+    for i in indicies:
+        check = []
+
+        neighbourhood = neighbours(array, i)
+        for j in neighbourhood:
+            check.append(j[3])
+
+        if len(np.argwhere(np.array(check)>0)) != 2:
+            status -= 1
+
+    return status
+
+@njit(parallel = True)
+def crumple(array):
+    '''
+    Markov Chain method to enforce movement toward more crumpled structure.
+    Defines energy as sum of distance between all indicies
+    '''
+
+    crossing_array = array.copy()
+    indicies = np.argwhere(crossing_array>0)
+    dist = 0
+
+    for i in indicies:
+        for j in indicies:
+            if i[0] != j[0] or i[1] != j[1] or i[2] != j[2]:
+                dist += (i[0] - j[0]) **2 + (i[1] - j[1]) **2 + (i[2] - j[2]) **2
+
+    energy = dist **(1/2)
+
+    return -energy
+
+def shear(array):
+    return array
+
+
+@njit(parallel=True)
+def lattice_writhe(array):
+    '''
+    Want to explore Tait numbers T(A_{i}) on the two areas (8 areas modulo symmetry) on the indicatrix corresponding to projections on: 
+    (x, z) plane, (y, z) plane.
+    Additionally, need to have defined direction to capture +,- crossings: cross product of a fixed orientation along knot
+    '''
+
+    indicies = np.argwhere(array>0)
+    TA_1 = 0
+    TA_2 = 0
+
+    for i in indicies:
+        '''
+        This gives you x, y coordinates of places in projection where crossings occur.
+        Nb. limited height so triple crossings don't occur
+        Need to build vector for each strand in crossing (3 points) and take cross product
+        Also need to define orientation for these vectors
+        Current issue: what to do when triple crossing? (lines 257, 264, 319, 312 ignore issue for now)
+        : solution - define 'good' projections (0, 1, 2) points, omit any other projections
+        '''
+
+        projected_vector_yz = array[:, i[1], i[2]] # list on yz plane projections
+        projected_vector_xz = array[i[0], :, i[2]] # list on xz plane projections
+
+        points_yz = np.argwhere(projected_vector_yz > 0) # gives two locations (x) of crossings
+        points_xz = np.argwhere(projected_vector_xz > 0) # gives two locations (y) of crossings
+
+        if len(points_yz) == 2: # unsure as to why this doesnt omit the other projections?
+
+            vec_1_yz = np.empty((2, 4))
+            vec_2_yz = np.empty((2, 4))
+
+            idx_1_yz = 0
+            idx_2_yz = 0
+
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    for dz in [-1, 0, 1]:
+
+                        if dx == 0 and dy == 0 and dz == 0:
+                            continue
+
+                        nx_1, ny_1, nz_1 = points_yz[0].item() + dx, i[1] + dy, i[2] + dz
+                        nx_2, ny_2, nz_2 = points_yz[1].item() + dx, i[1] + dy, i[2] + dz
+
+                        if 0<= nx_1 <array.shape[0] and 0<= ny_1 <array.shape[1] and 0<= nz_1 <array.shape[2] and array[nx_1][ny_1][nz_1] > 0:
+
+                            if idx_1_yz < 2:
+                                vec_1_yz[idx_1_yz] = [nx_1, ny_1, nz_1, array[nx_1][ny_1][nz_1]]
+
+                            idx_1_yz +=1
+
+                        if 0<= nx_2 <array.shape[0] and 0<= ny_2 <array.shape[1] and 0<= nz_2 <array.shape[2] and array[nx_2][ny_2][nz_2] > 0:
+
+                            if idx_2_yz < 2:
+                                vec_2_yz[idx_2_yz] = [nx_2, ny_2, nz_2, array[nx_2][ny_2][nz_2]]
+
+                            idx_2_yz += 1
+
+            # define vector to point small -> large
+            p1 = vec_1_yz[0]
+            p2 = vec_1_yz[1]
+            p3 = vec_2_yz[0]
+            p4 = vec_2_yz[1]
+
+            arrow_1 = [p1[0] - p2[0], p1[1] - p2[1], p1[2] - p2[2]]
+            arrow_2 = [p3[0] - p4[0], p3[1] - p4[1], p3[2] - p4[2]]
+
+            arrow_1 = np.array(arrow_1)
+            arrow_2 = np.array(arrow_2)
+
+            if p4[3]>p3[3]:
+                arrow_2 = -1 * arrow_2
+            
+            if p2[3]>p1[3]:
+                arrow_1 = -1 * arrow_1
+
+            cross_prod = np.cross(arrow_1, arrow_2)
+            mag = ((arrow_1[0]-arrow_2[0])**2 + (arrow_1[1]-arrow_2[1])**2 +(arrow_1[2]-arrow_2[2])**2)**(1/2)
+            sign = np.sign(cross_prod[0])
+            if mag == 0:
+                TA_1 += sign
+            
+            else:
+                TA_1 += sign
+
+        if len(points_xz) == 2:
+
+            vec_1_xz = np.empty((2, 4))
+            vec_2_xz = np.empty((2, 4))
+
+            idx_1_xz = 0
+            idx_2_xz = 0
+
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    for dz in [-1, 0, 1]:
+
+                        if dx == 0 and dy == 0 and dz == 0:
+                            continue
+
+                        nx_1, ny_1, nz_1 = i[0] + dx, points_xz[0].item() + dy, i[2] + dz
+                        nx_2, ny_2, nz_2 = i[0] + dx, points_xz[1].item() + dy, i[2] + dz
+
+                        if 0<= nx_1 <array.shape[0] and 0<= ny_1 <array.shape[1] and 0<= nz_1 <array.shape[2] and array[nx_1][ny_1][nz_1] > 0:
+
+                            if idx_1_xz < 2:
+                                vec_1_xz[idx_1_xz] = [nx_1, ny_1, nz_1, array[nx_1][ny_1][nz_1]]
+
+                            idx_1_xz +=1
+
+                        if 0<= nx_2 <array.shape[0] and 0<= ny_2 <array.shape[1] and 0<= nz_2 <array.shape[2] and array[nx_2][ny_2][nz_2] > 0:
+
+                            if idx_2_xz < 2:
+                                vec_2_xz[idx_2_xz] = [nx_2, ny_2, nz_2, array[nx_2][ny_2][nz_2]]
+
+                            idx_2_xz += 1
+
+            # define vector to point small -> large
+            p1 = vec_1_xz[0]
+            p2 = vec_1_xz[1]
+            p3 = vec_2_xz[0]
+            p4 = vec_2_xz[1]
+
+            arrow_1 = [p1[0] - p2[0], p1[1] - p2[1], p1[2] - p2[2]]
+            arrow_2 = [p3[0] - p4[0], p3[1] - p4[1], p3[2] - p4[2]]
+
+            arrow_1 = np.array(arrow_1)
+            arrow_2 = np.array(arrow_2)
+
+            if p4[3]>p3[3]:
+                arrow_2 = -1 * arrow_2
+            
+            if p2[3]>p1[3]:
+                arrow_1 = -1 * arrow_1
+
+            cross_prod = np.cross(arrow_1, arrow_2)
+            sign = np.sign(cross_prod[1])
+            TA_2 += sign
+
+    return (TA_1 + TA_2)/ 4
+
+
+@njit(parallel = True)
+def metropolis_acceptance(old, new, temperature):
+    '''
+    Probability to randomly accept update moves that arent crossing increasing.
+    '''
+    if new > old:
+        return True
+    else:
+        prob = np.exp((new-old)/temperature)
+        return np.random.uniform(0, 1) < prob
+    
+def BFACF_update(array, temperature, time):
+    '''
+    BFACF algorithm on oriented curve
+    '''
+
+    update_array = array.copy()
+
+    # old_energy = compute_energy(update_array)
+
+    old_c_energy = crumple(update_array)
+    old_energy = lattice_writhe(update_array)
+
+    valid_indicies = np.argwhere(array > 1)
+
+    random_edge = np.random.choice(len(valid_indicies))
+    new_edge = find_new(update_array, valid_indicies[random_edge])
+
+    update_array[valid_indicies[random_edge][0]][valid_indicies[random_edge][1]][valid_indicies[random_edge][2]] = 0
+
+    update_array[int(new_edge[0])][int(new_edge[1])][int(new_edge[2])] += array[valid_indicies[random_edge][0]][valid_indicies[random_edge][1]][valid_indicies[random_edge][2]]
+
+    status = check_verticies(update_array)
+
+    # new_energy = compute_energy(update_array)
+
+    new_c_energy = crumple(update_array)
+    new_energy = lattice_writhe(update_array)
+
+    if status < 0:
+        return array, 0
+    
+    else:
+        if time< 0:
+            return update_array, new_energy
+        
+        elif 0<time<5000:
+            if metropolis_acceptance(old_c_energy, new_c_energy, temperature):
+                return update_array, new_energy
+            else:
+                return array, 0
+        
+        else:
+            if metropolis_acceptance(old_energy, new_energy, temperature):
+                return update_array, new_energy
+            else:
+                return array, 0
+
+
+def main(discretization, temperature, it):
+
+    # set_num_threads(12)
+
+    animate3D = False
+    plot = True
+    state_space = np.zeros((16, discretization, discretization))
+
+    unknot = k0_1_initialization(state_space)
+
+    print('Orienting...')
+    unknot = orient(unknot)
+    writhe = lattice_writhe(unknot)
+    print('Initial update...')
+    unknot, energy = BFACF_update(unknot, temperature, 0)
+
+    # run BFACF for a bunch of timesteps
+    writhe_calc = []
+    for i in range(it):
+        unknot, energy = BFACF_update(unknot, temperature, i)
+        writhe_calc.append(energy)
+        if i%1000 == 0:
+            print(f"simulation: {i/it}%")
+
+    coords = np.argwhere(unknot>0)
+    coord_dat = [(unknot[i[0], i[1], i[2]], i[0], i[1], i[2]) for i in coords]
+
+    np.savetxt('config.csv', coord_dat, delimiter=",", fmt='%d')
+
+    writhe_calc = [x for x in writhe_calc if x != 0]  # O(n)
+    print(writhe_calc)
+
+    plt.hist(writhe_calc)
+    plt.savefig('writhe_distn')
+
+    if plot == True:
+
+        norm = mcolors.Normalize(vmin=np.min(unknot[unknot > 0]), vmax=np.max(unknot))
+        cmap = cm.coolwarm  
+
+        # Initialize color array
+        colors = np.zeros(unknot.shape + (4,))  # RGBA color array
+
+        # Apply colormap for nonzero values
+        mask = unknot > 0  
+        colors[mask] = cmap(norm(unknot[mask]))  
+
+        colors[..., 3] = np.where(unknot > 0, 1.0, 0.0)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        # Plot the voxels
+        ax.voxels(unknot > 0, facecolors=colors)
+
+        ax.set_xlim([0, 100])
+        ax.set_ylim([0, 100])
+        ax.set_zlim([0, 100])
+
+        plt.savefig('tangle')
+        plt.show()
+
+    if animate3D == True:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        def update(frame):
+            global unknot
+            for i in range(1000):  # Perform multiple updates per frame
+                unknot = BFACF_update(unknot, temperature) 
+            
+            ax.clear()  # Clear previous voxels
+
+            # Get voxel positions (nonzero values)
+            filled = unknot > 0
+            ax.voxels(filled, facecolors='blue', edgecolors='black', alpha=0.5)
+            ax.set_xlim([0, 100])
+            ax.set_ylim([0, 100])
+            ax.set_zlim([0, 100])
+
+            ax.set_title(f"Unknot")  # Optional title update
+
+            return ax,
+
+        ani = animation.FuncAnimation(fig, update, frames=10, interval=500, blit=False)
+        plt.show()
+
+
+main(discretization= 100, temperature=0.1, it= 10000)
---
+++ knot_init.py
@@ -0,0 +1,121 @@
+import numpy as np
+from numba import njit, prange
+import matplotlib.pyplot as plt
+
+def draw_line(grid, z, x1, y1, x2, y2, size):
+    '''
+    Bresenham's line algorithm; draws lines between points in array
+    '''
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    sx = 1 if x1 < x2 else -1
+    sy = 1 if y1 < y2 else -1
+    err = dx - dy
+
+    while True:
+        if 0 <= x1 < size and 0 <= y1 < size:
+            grid[z, y1, x1] = 1
+        if x1 == x2 and y1 == y2:
+            break
+        e2 = err * 2
+        if e2 > -dy:
+            err -= dy
+            x1 += sx
+        if e2 < dx:
+            err += dx
+            y1 += sy
+
+
+def k0_1_initialization(array):
+    '''
+    Initializes an unknot on the boundary of the state space i.e.
+    '''
+
+    length = len(array[0])
+    for i in prange(50, length - 24):
+        for j in prange(50, length - 24):
+            if i == 50 or i == length-25:
+                array[4][i][j] = 1
+
+            if j == 50 or j == length-25:
+
+                array[4][i][j] = 1
+
+    array[4][50][50] = array[4][50][75] = array[4][75][75] = array[4][75][50] = 0
+
+    print(len(np.argwhere(array == 1)))
+
+    return array
+
+
+def k0_1_initialization_2(array):
+    '''
+    Initializes an thin unknot on the boundary of the state space i.e.
+    '''
+
+    length = len(array[0])
+    for i in prange(25, length - 24):
+        for j in prange(70, length - 24):
+            if i == 25 or i == length-25:
+                array[4][i][j] = 1
+
+            if j == 70 or j == length-25:
+
+                array[4][i][j] = 1
+
+    array[4][25][75] = array[4][75][70] = array[4][75][75] = array[4][25][70] = 0
+
+    print(len(np.argwhere(array == 1)))
+
+    return array
+
+
+def k3_1_initialization(array):
+    '''
+    Initializes a 3_1 in state space
+    '''
+
+    draw_line(array, 6, 51, 49, 51, 38, 100) # 6 
+    draw_line(array, 6, 50, 37, 44, 37, 100) # 6
+
+    array[5][37][43] = array[4][37][43] = array[3][37][43] = 1
+
+    draw_line(array, 2, 43, 38, 43, 45, 100) # 2
+
+    array[5][46][43] = array[4][46][43] = array[3][46][43] = 1
+
+    draw_line(array, 6, 43, 47, 43, 52, 100) # 6 dw
+    draw_line(array, 6, 44, 53, 52, 53, 100) # 6
+    draw_line(array, 6, 53, 52, 53, 41, 100) # 6
+
+    array[5][40][53] = 1 # 5
+
+    draw_line(array, 4, 52, 40, 41, 40, 100) # 4 dw
+    draw_line(array, 4, 40, 41, 40, 49, 100) # 4
+    draw_line(array, 4, 41, 50, 49, 50, 100) # 4
+
+    array[5][50][50] = 1    
+
+    return array
+
+
+def k4_1_initialization(array):
+    '''
+    Initializes a 4_1 in state space
+    '''
+
+    return array
+
+def k5_1_initialization(array):
+    '''
+    Initializes a 5_1 in state space
+    '''
+
+    return array
+
+def k5_2_initialization(array):
+    '''
+    Initializes a 5_2 in state space
+    '''
+
+    return array

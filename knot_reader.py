e5d9c17d18371d64bff02fdb4d9f5ca37e9e3ec5
Djordje <djmihajlovic1@gmail.com>
2025-04-24 11:13:25 +0100

    fix read

--- knot_reader.py
+++ knot_reader.py
@@ -4,13 +4,10 @@ import matplotlib.cm as cm
 import matplotlib.colors as mcolors  
 from knot_init import plot_3d
 
-knot = np.loadtxt('/Users/s1910360/Desktop/0_1_445.csv', delimiter=',', dtype=int)
+# knot = np.loadtxt('/Users/s1910360/Desktop/0_1_445.csv', delimiter=',', dtype=int)
 
 def read(knot):
     state = np.zeros((100, 100, 100), dtype=np.int64)
     for i in knot:
         state[i[1]][i[2]][i[3]] = i[0]
     return state
-
-state = read(knot)
-plot_3d(state)

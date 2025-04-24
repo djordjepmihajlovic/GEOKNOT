76b6bc68dfb27023b15e70bcee9c11e47b81140f
Djordje <djmihajlovic1@gmail.com>
2025-04-24 21:41:44 +0100

    reader

--- knot_reader.py
+++ knot_reader.py
@@ -5,7 +5,7 @@ import matplotlib.colors as mcolors
 from knot_init import *
 from defunct.knot_evolution import lattice_writhe_Klenin
 
-knot = np.loadtxt('/Users/s1910360/Desktop/0_1_8_5.csv', delimiter=',', dtype=int)
+# knot = np.loadtxt('/Users/s1910360/Desktop/0_1_8_5.csv', delimiter=',', dtype=int)
 
 def read(knot):
     state = np.zeros((100, 100, 100), dtype=np.int64)
@@ -13,8 +13,8 @@ def read(knot):
         state[i[1]][i[2]][i[3]] = i[0]
     return state
 
-im = lattice_writhe_Klenin(read(knot))
-print(np.sum(im))
-plot_3d(read(knot))
-plt.imshow(im)
-plt.show()
+# im = lattice_writhe_Klenin(read(knot))
+# print(np.sum(im))
+# plot_3d(read(knot))
+# plt.imshow(im)
+# plt.show()

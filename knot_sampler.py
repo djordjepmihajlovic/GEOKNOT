ed9ea83fba9290e7b62154fbd71589315f8609ee
Djordje <djmihajlovic1@gmail.com>
2025-06-23 15:45:39 +0100

    bug 2

--- knot_sampler.py
+++ knot_sampler.py
@@ -116,7 +116,7 @@ def wang_landau_sampling(partition, oriented, knot_type, writhe_bins, entang_bin
                             w = [wx[0] for wx in elements]
                             new_coord_w = [(w[idx],) + coord for idx, coord in enumerate(new_coord)]
 
-                            np.savetxt(f'samples/{knot_type}_{partition}_{int(writhe_ranges[i])}_{int(entang_ranges[j])}.csv', new_coord_w, delimiter=",", fmt='%.5f')
+                            np.savetxt(f'samples/{knot_type}_{partition}_{int((writhe_ranges[i][0]+writhe_ranges[i][1])/2)}_{int((entang_ranges[j][0]+entang_ranges[j][1])/2)}.csv', new_coord_w, delimiter=",", fmt='%.5f')
                             sampled_states.append([partition, int((writhe_ranges[i][0]+writhe_ranges[i][1])/2), int((entang_ranges[j][0]+entang_ranges[j][1])/2)])
             
     return sampled_states
